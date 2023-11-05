7ef4c5c3f1ac9a4deae85eb3654a44cc5350c4fd
Valentina Patova-Staninska <102672076+valiapatova@users.noreply.github.com>
2023-11-05 19:02:57 +0200

    Add UserListTable Search components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,22 @@
 import Footer from './components/Footer.jsx'
 import Header from './components/Header.jsx'
+import UserList from './components/UserList.jsx'
 import './styles.css'
 
 
 function App() {
- 
+
   return (
     <>
-      <h1>hello</h1>
-      <Header/>
+      <Header />
 
-      <Footer/>
+      {/* <!-- Main component  --> */}
+      
+      <main className="main">
+        <UserList />
+      </main>
+
+      <Footer />
     </>
   )
 }
