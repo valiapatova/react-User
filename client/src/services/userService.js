a22728657068892a320818d487134871c786206c
Valentina Patova-Staninska <102672076+valiapatova@users.noreply.github.com>
2023-11-10 12:29:16 +0200

    userService

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -19,35 +19,56 @@ export const getAll = async () => {
 
 };
 
+export const getOne = async (userId) => {
+    const response = await fetch(`${baseUrl}/${userId}`);
+    const result = await response.json();
+
+    return result;
+};
+
+
 export const create = async (data) => {
 
     const body = {
-        firstName:data.firstName,
-        lastName:data.lastName,
-        email:data.email,
-        imageUrl:data.imageUrl,
-        phoneNumber:data.phoneNumber,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        email: data.email,
+        imageUrl: data.imageUrl,
+        phoneNumber: data.phoneNumber,
         createdAt: new Date().toISOString(),
         updatedAt: new Date().toISOString(),
         address: {
-            country:data.country,
-            city:data.city,
-            street:data.street,
-            streetNumber: data.streetNumber        
+            country: data.country,
+            city: data.city,
+            street: data.street,
+            streetNumber: data.streetNumber
         }
     };
 
-    const response = await fetch(baseUrl,{
+    const response = await fetch(baseUrl, {
         method: 'POST',
         headers: {
-            'Content-Type':'applicatoon/json',
+            'Content-Type': 'applicatoon/json',
         },
         body: JSON.stringify(body),
     });
 
-    const result=await response.json() ;
+    const result = await response.json();
     console.log(result);
 
-    return result ;
+    return result;
+};
+
+export const remove = async (userId) => {
+    const response = await fetch(`${baseUrl}/${userId}`, {
+        method: 'DELETE'
+    });
+
+
+    const result = await response.json();
+
+    return result;
 
 };
+
+
